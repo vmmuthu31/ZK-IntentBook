655fa12bc0ba1e0ee5db1dd43e336ea99c6c731a
Vairamuthu <mvairamuthu2003@gmail.com>
2026-02-04 16:18:32 +0530

    feat: Add security module exports for encryption functionalities

---
+++ src/server/security/index.ts
@@ -0,0 +1,13 @@
+export {
+  generateKeyPair,
+  encryptIntent,
+  decryptIntent,
+  generateCommitmentHash,
+  createEncryptedIntent,
+  decryptEncryptedIntent,
+  hexToPublicKey,
+  publicKeyToHex,
+  verifyCommitment,
+  type EncryptionKeyPair,
+  type EncryptionResult,
+} from "./encryption";
